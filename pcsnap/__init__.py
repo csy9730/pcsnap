3d0b4c29e3b0215966a67c2afc480dca411acf5c
chensy_zlg <gd_csy@163.com>
2020-10-30 09:51:32 +0800

    add setup.py

---
+++ pcsnap/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+__name__ = "pcsnap"
+__version__ = '0.0.1'
+__author__ = "StartRise"
+__description__ = "pc information collection kit"
+__license__='---'
+__keywords__ = ['pc', 'information']
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+from io import open
+from setuptools import setup, find_packages
+import pcsnap
+
+fpk=find_packages(where=".")
+print(fpk)
+
+setup(
+    name='pcsnap',
+    version=pcsnap.__version__,
+    description=pcsnap.__description__,
+    author=pcsnap.__author__,
+    packages=fpk,
+
+    zip_safe=False,
+
+    keywords=pcsnap.__keywords__,
+    python_requires='>=3.6',
+    setup_requires=[],
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pcsnap=pcsnap.__main__:main'
+        ]
+    }
+
+)
